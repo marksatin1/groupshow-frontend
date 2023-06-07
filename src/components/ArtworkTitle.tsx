191e0592432f48c6120ae5783034a76bf5d58998
RafaelCaso <94573618+RafaelCaso@users.noreply.github.com>
2023-06-07 09:17:55 -0600

    add component files and boilerplate

---
+++ src/components/ArtworkTitle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArtworkTitle = () => {
+  return (
+    <div>ArtworkTitle</div>
+  )
+}
+
+export default ArtworkTitle
---
+++ src/components/VideoDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoDisplay = () => {
+  return (
+    <div>VideoDisplay</div>
+  )
+}
+
+export default VideoDisplay

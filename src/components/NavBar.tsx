5d52b7efaca44c46c003f9d11d8e9b806b85aa0e
Mark Satin <msatin2009@gmail.com>
2023-06-08 15:30:55 -0400

    connected Portfolio to backend

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import ProfilePicture from './ProfilePicture'
-import SearchBar from './SearchBar'
-import HamburgerMenu from './HamburgerMenu'
+import React from "react";
+import ProfilePicture from "./ProfilePicture";
+import SearchBar from "./SearchBar";
+import HamburgerMenu from "./HamburgerMenu";
 
 const NavBar = () => {
   return (
-
     <div>
       <ProfilePicture />
       <SearchBar />
       <HamburgerMenu />
     </div>
-    
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;

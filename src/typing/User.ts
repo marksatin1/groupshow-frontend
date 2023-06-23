c378a575411897c797808e4c2122c703e36e6d17
Mark Satin <msatin2009@gmail.com>
2023-06-22 22:04:34 -0400

    fixed state handling, typing for all authCtx routes

--- src/typing/User.ts
+++ src/typing/User.ts
@@ -18,10 +18,3 @@ export interface IUser {
   videos?: IVideo[];
   writings?: IWriting[];
 }
-
-export interface IUserDetails {
-  currentUser: IUser | void;
-  jwtAccessToken: string | void;
-  jwtRefreshToken: string | void;
-  jwtRefreshExpDateMs: number | void;
-}
